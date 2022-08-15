9e8cf781d17a4121d25301c751299a5c1ff5c3fd
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2022-08-15 18:54:26 +0530

    ex05: IMG asset

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,40 @@
 const path = require("path");
 
 const webpackConfig = {
-	entry: path.resolve(__dirname, "src", "index.js"),
+  entry: path.resolve(__dirname, "src", "index.js"),
 
-	output: {
-		filename: "main.js",
-		path: path.resolve(__dirname, "dist")
-	},
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 
-    mode: "production",
-    
-    module: {
-		rules: [
-			{
-				// babel rules
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-env"]
-					}
-				}
-			},
-			{
-				// css rules
-				test: /\.css$/i,
-				use: ["style-loader", "css-loader"]
-			},
+  mode: "production",
 
-		]
-	},
-
+  module: {
+    rules: [
+      {
+        // babel rules
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        // css rules
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        // img assets rules
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
 
 module.exports = webpackConfig;
